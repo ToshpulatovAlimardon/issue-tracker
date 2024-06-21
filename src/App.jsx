c4d3782db9b5636fdb205e1c538d256aca93a68d
ToshpulatovAlimardon <alimardontoshpulatov001@gmail.com>
2024-06-21 23:23:07 +0500

    added search component

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Search from "./components/shared/search";
 
 function App() {
   return (
-    <div className="mx-auto max-w-[1100px] pt-12">
+    <div className="mx-auto max-w-[1100px] pt-24">
       <h1 className="text-5xl font-bold text-center">Issue Tracker</h1>
 
-      <div>
+      <div className="pt-12">
         <div>
           <Search/>
         </div>
--- src/components/shared/search.jsx
+++ src/components/shared/search.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Input } from '../ui/input'
+import { Label } from '../ui/label'
 
 const Search = () => {
   return (
-    <div>
-      <span>Search Issues</span>
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Label htmlFor="search">Search Issues</Label>
+      <Input type="search" id="search" placeholder="Search" />
     </div>
   )
 }
