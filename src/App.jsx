0a5d66a00a08efd89fe1fd7fffd62b5d1d7687cd
ToshpulatovAlimardon <alimardontoshpulatov001@gmail.com>
2024-06-21 23:05:33 +0500

    added title

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import { Button } from "./components/ui/button";
+import Search from "./components/shared/search";
+
 
 function App() {
-  return <Button>BUTTON</Button>;
+  return (
+    <div className="mx-auto max-w-[1100px] pt-12">
+      <h1 className="text-5xl font-bold text-center">Issue Tracker</h1>
+
+      <div>
+        <div>
+          <Search/>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/shared/search.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div>
+      <span>Search Issues</span>
+    </div>
+  )
+}
+
+export default Search
